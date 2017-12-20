82fac54df746a30295aeda149659a1810782e8ae
Isaias Faria - Serveux ISIMA <isaias@isaiasfaria.com.br>
2017-12-20 10:49:27 +0100

    First Commit - Configuring the workspace

---
+++ Class1/Source Code/main.cpp
@@ -0,0 +1,35 @@
+/*
+
+[2017] Isaias Faria Silva 
+All Rights Reserved.
+ 
+NOTICE: All information contained here in this file is the 
+        property of Isaias Faria Silva
+        If you want to use this, please put the contact 
+        rights information.
+ 
+PURPOSE:Practical activity of Algorithm for images
+		Clermont-Auvergne University - Computer Science Institute
+		ZZ3 - 2017 december 
+
+DESCRIPTION:
+		This is the main file of this project
+
+Last update: 20 december 2017
+*/
+
+
+
+
+// TP_X Imagerie - Prise en main de la librairie CImg
+
+#include "CImg.h"
+using namespace cimg_library;
+
+/******************************************
+Main
+******************************************/
+int main(int argc,char **argv) 
+{ 
+
+}
